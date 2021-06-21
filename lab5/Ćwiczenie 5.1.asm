67b73c653cfd979c554cbf168c1c8167f2fa4518
adamzielina <56132235+adamzielina@users.noreply.github.com>
2021-06-21 17:45:49 +0200

    Create Ćwiczenie 5.1.asm

---
+++ lab5/Ćwiczenie 5.1.asm
@@ -0,0 +1,9 @@
+
+start:
+	ldi r16, -9
+	ldi r17, -57
+	ldi r18, -101
+	rjmp start
+; -9 reprezentowane jako 0xf7, binarnie 11110111
+; -57 reprezentowane jako 0xc7, binarnie 11000111
+; -101 reprezentowane jako 0x9b, binarnie 10011011
